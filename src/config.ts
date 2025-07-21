d610eabb59a3f6fa8719b05569076b6959f5fc1b
xagunel <159116090+sjhxlll@users.noreply.github.com>
2025-07-21 17:36:25 +0800

    change config
    first change config and my first article

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ import { LinkPreset } from "./types/config";
 export const siteConfig: SiteConfig = {
 	title: "Fuwari",
 	subtitle: "Demo Site",
-	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/sjhxlll", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
---
+++ src/content/posts/my-first-post.md
@@ -0,0 +1,13 @@
+---
+title: 我的第一篇可爱博客
+published: 2025-07-21
+description: 这是我的第一篇文章，好开心呀！
+image: ./cover.jpg   # 如果有封面图，需要先把图片也传到这个目录下
+tags: [心情, 日记]
+category: 生活
+draft: false
+---
+
+这里是你的正文内容，用 Markdown 语法写哦~
+
+Hello World! 我是小可爱！
