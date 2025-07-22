8c97f629964164264f07f307d96a517dab704d96
xagunel <159116090+sjhxlll@users.noreply.github.com>
2025-07-22 11:17:10 +0800

    friends

--- src/config.ts
+++ src/config.ts
@@ -44,6 +44,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
+		LinkPreset.Friends,  
 		{
 			name: "GitHub",
 			url: "https://github.com/sjhxlll", // Internal links should not include the base path, as it is automatically added
