0a455164505ec5946a35ed7c8ea9053c4eccbb47
xagunel <159116090+sjhxlll@users.noreply.github.com>
2025-07-22 15:49:40 +0800

    fiax

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.diwuren.com",
+	site: "https://www.aidoi.top",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
