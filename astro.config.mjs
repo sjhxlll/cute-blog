75f930e4d9f11cf71b09ff81a2a26eb7cccbbc2f
xagunel <159116090+sjhxlll@users.noreply.github.com>
2025-07-29 10:16:43 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.aidoi.top",
+    site: "https://www.xomoe.cn",
     base: "/",
     trailingSlash: "always",
     integrations: [
